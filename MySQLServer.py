050125936b3b2d322e4e192006d63a523bae0c9e
unknown <bons6710hos@gmail.com>
2025-06-23 21:31:13 +0300

    build database

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+import pymysql
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
+Host = os.getenv('HOST')
+User = os.getenv('USER')
+Password = os.getenv('PASSWORD')
+database_name= "alx_book_store"
+try:
+    mydb =pymysql.Connect(
+        host=Host,
+        user=User,
+        password=Password,
+        # database=database_name
+    )
+    
+    cursor = mydb.cursor()
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name};")
+    mydb.commit()
+    
+
+
+    print(f"Database {database_name} created successfully!")
+except pymysql.MySQLError as e:
+    print(f"Error creating database: {e}")
+    
